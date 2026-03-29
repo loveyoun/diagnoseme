01594d0faeee534ac15f135ba2c667f8a352d14a
loveyoun <lovekite0406@naver.com>
2026-03-30 02:42:43 +0900

    :hammer: Update docker-compose.yml for redis

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 from enum import StrEnum
 
-from tortoise import fields, models
+from tortoise import fields
+
+from app.models.common import Common
 
 
 class Gender(StrEnum):
@@ -9,7 +11,7 @@ class Gender(StrEnum):
     UNKNOWN = "UNKNOWN"
 
 
-class User(models.Model):
+class User(Common):
     user_id = fields.BigIntField(primary_key=True)
     nickname = fields.CharField(max_length=10)
     email = fields.CharField(max_length=40, null=True)
@@ -19,13 +21,13 @@ class User(models.Model):
     naver_id = fields.CharField(max_length=100, unique=True)
 
     # 네이버 제공 정보
-    name = fields.CharField(max_length=100, null=True)          # 회원 이름
-    email = fields.CharField(max_length=255, null=True)         # 연락처 이메일
-    nickname = fields.CharField(max_length=100, null=True)      # 별명
-    gender = fields.CharField(max_length=1, null=True)          # 성별 (M/F/U)
-    birthday = fields.CharField(max_length=5, null=True)        # 생일 (MM-DD)
-    birthyear = fields.CharField(max_length=4, null=True)       # 출생연도 (YYYY)
-    profile_image = fields.CharField(max_length=500, null=True) # 프로필 사진 URL
+    name = fields.CharField(max_length=100, null=True)  # 회원 이름
+    email = fields.CharField(max_length=255, null=True)  # 연락처 이메일
+    nickname = fields.CharField(max_length=100, null=True)  # 별명
+    gender = fields.CharField(max_length=1, null=True)  # 성별 (M/F/U)
+    birthday = fields.CharField(max_length=5, null=True)  # 생일 (MM-DD)
+    birthyear = fields.CharField(max_length=4, null=True)  # 출생연도 (YYYY)
+    profile_image = fields.CharField(max_length=500, null=True)  # 프로필 사진 URL
 
     class Meta:
         table = "users"
